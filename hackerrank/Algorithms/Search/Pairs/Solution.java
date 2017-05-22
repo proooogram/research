872cb704d47339ce63d4be9a969d3ae6cc399af4
shcaoguilin <shcaoguilin@sina.com>
2017-05-22 16:24:45 +0800

    添加说明

--- hackerrank/Algorithms/Search/Pairs/Solution.java
+++ hackerrank/Algorithms/Search/Pairs/Solution.java
@@ -4,6 +4,27 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+/**
+
+a : 1 5 3 4 2
+k : 2
+求数组a中两元素差的绝对值为k, 符合条件的元素对个数
+
+-k:		-1		3		1		2		0		:  ak: add k
+a : 		 1		5		3		4		2
++k:		 3		7		5		6		4		:  sk : substract k 
+
+找ak和a中相同元素, 找sk和a中相同元素,  个数相加
+同样的被重复了一次,    所以除以2, 得到结果
+
+将a 反转为f:  a的内容作为f的下标, f的内容表示标记这里有a的一个元素   ：  
+	注意，由于a起点可能比较大， 所以要将a统统减速a的最小元素min，这样f尽可能小、尽可能密集.   
+然后ak到f中查找, sk到f中查找, 计数
+
+注意: 如果两层循环遍历的话 消耗时间不符合hackerrank的要求
+样本16是此方案的陷进， ：  a非常分散， 总共只有10个元素，直接内存不够, 
+就到这了
+*/
 public class Solution {
     static int pairs(int[] a,int k) {
         int c = 0;
