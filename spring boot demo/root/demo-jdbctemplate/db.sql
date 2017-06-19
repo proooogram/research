6fbf53f152414ff9fbb9423f7045aab07706758d
shcaoguilin <shcaoguilin@sina.com>
2017-06-19 12:55:25 +0800

    补充提交

---
+++ spring boot demo/root/demo-jdbctemplate/db.sql
@@ -0,0 +1,2 @@
+create database db0 default charset utf8;
+grant all privileges  on db0.* to 'root'@'%' identified by 'root';
---
+++ spring boot demo/root/demo-jdbctemplate/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table user(
+id int  auto_increment primary key ,
+username varchar(32) ,
+password varchar(32),
+email varchar(32)
+) ;
+
